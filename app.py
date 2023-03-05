d46165f81f3495e403ddc53b2ccb78f6ddc71b71
Thiago Cardoso <cafeprathi@gmail.com>
2023-03-05 20:50:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from datetime import datetime, timezone
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-#from selenium.webdriver.common.options import Options
+from selenium.webdriver.common.options import Options
 from selenium.common.exceptions import *
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -34,5 +34,33 @@ def new_product(sql, connection, name, price, site, quote_date, link_img):
     else:
         print("Dados ja cadastrados anteriormente")
 
-new_product(sql, connection, 'Corsarebaixado', 10000, 'Amazon.com/corsa', datetime.now(), 'https://source.unsplash.com/random/150x150' )
+#new_product(sql, connection, 'Corsarebaixado', 10000, 'Amazon.com/corsa', datetime.now(), 'https://source.unsplash.com/random/150x150' )
 connection.commit()
+
+def init_driver():
+    chrome_options = Options()
+
+    arguments = ['--lang-US', '--window-size=1920,180', '--incognito']
+
+    for argument in arguments:
+        chrome_options.add_argument(argument)
+
+    chrome_options.add_experimental_option('prefs', {
+        'download.prompt_for_download': False,
+        'profile.defalt_content_setting_values.notifications': 2,
+        'profile.defaut_content_setting_values.automatic_downloads': 1
+    })
+
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+    wait = WebDriverWait(driver, 10, poll_frequency=1, ignored_exceptions=[NoSuchElementException, ElementNotVisibleException, ElementNotSelectableException])
+
+    return driver, wait
+
+def web_scraping_amazon():
+    pass
+
+def web_scraping_magalu():
+    pass
+
+def web_scraping_fastshop():
+    pass
